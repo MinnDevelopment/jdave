03c0986207586ec593b6821c18428bc8b81cec3d
Florian Spieß <business@minn.dev>
2026-01-11 20:05:04 +0100

    Migrate to downloaded artifacts instead of submodule builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "club.minnced"
-version = "0.1.0"
+version = "0.1.1"
 
 java {
     toolchain {
--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -1,6 +1,13 @@
+import de.undercouch.gradle.tasks.download.Download
+import java.nio.file.Files
+import jdave.gradle.Architecture
+import jdave.gradle.OperatingSystem
 import jdave.gradle.getPlatform
 
-plugins { `publishing-environment` }
+plugins {
+    `publishing-environment`
+    alias(libs.plugins.download)
+}
 
 publishingEnvironment { moduleName = "jdave-native-${getPlatform()}" }
 
@@ -13,13 +20,41 @@ dependencies {
     testImplementation(libs.logback)
 }
 
+fun getArtifactName(): String {
+    return getPlatform().run {
+        when {
+            operatingSystem == OperatingSystem.Windows && arch == Architecture.X86_64 ->
+                "Windows-x86_64.zip"
+            operatingSystem == OperatingSystem.Linux && arch == Architecture.X86_64 ->
+                "Linux-x86_64.zip"
+            operatingSystem == OperatingSystem.Linux && arch == Architecture.AARCH64 ->
+                "Linux-aarch64.zip"
+            operatingSystem == OperatingSystem.MacOS -> "Darwin.zip"
+            else -> error("Unsupported platform: $this")
+        }
+    }
+}
+
+fun getArtifactDownloadUrl(): String {
+    return "https://github.com/MinnDevelopment/libdave/releases/download/v0.1.0/${getArtifactName()}"
+}
+
 val nativeResourceRoot = "resources/libdave"
+val downloadDir: Provider<Directory> = layout.buildDirectory.dir("downloads")
+
+val downloadNatives by
+    tasks.registering(Download::class) {
+        doFirst { Files.createDirectories(downloadDir.get().asFile.toPath()) }
+
+        src(getArtifactDownloadUrl())
+        dest(downloadDir)
+    }
 
 val assembleNatives by
     tasks.registering(Copy::class) {
-        dependsOn(gradle.includedBuild("libdave").task(":cpp:assemble"))
+        dependsOn(downloadNatives)
 
-        from(project.layout.projectDirectory.dir("libdave/cpp/build/libs")) {
+        from(zipTree(downloadDir.get().file(getArtifactName()))) {
             include {
                 it.name.endsWith(".so") || it.name.endsWith(".dll") || it.name.endsWith(".dylib")
             }
